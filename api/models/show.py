dde0477dfde83640869d1fe5ad205571545cc32b
NonsenseSynapse <keithnewman10@gmail.com>
2025-08-01 14:47:16 -0700

    pre-commit cleanup

--- api/models/show.py
+++ api/models/show.py
@@ -1,6 +1,8 @@
-from sqlalchemy import Column, DateTime, Integer, String, ForeignKey
-from api.db.database import DbBase
+from sqlalchemy import Column, DateTime, Integer, String
 from sqlalchemy.sql import func
+
+from api.db.database import DbBase
+
 
 class Show(DbBase):
     __tablename__ = "show"
